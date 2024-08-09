8fdc487672401c7ce4173d93049794de83b0d6cb
kwss-865 <134401209+kwss-865@users.noreply.github.com>
2024-08-09 10:37:19 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultado = document.querySelector('.caixa-resultado');
+const textoResultado = document.querySelector('texto-resultado');
+
+const perguntas = [
+    {
+        enunciado: "texto01",
+        alternativas: [
+            "Alternativas01",
+            "Alternativas02"
+        ]
+    },
+    {
+        enunciado: "texto02",
+        alternativas: [
+            "Alternativas01",
+            "Alternativas02"
+        ]  
+    }
+]
+
