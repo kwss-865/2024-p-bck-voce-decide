3ab633b9096f3ca06897390e7a184b9c258f3728
kwss-865 <134401209+kwss-865@users.noreply.github.com>
2024-08-14 20:57:53 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -18,6 +18,43 @@ const perguntas = [
             "Alternativas01",
             "Alternativas02"
         ]  
+    },
+    {
+        enunciado: "texto03",
+        alternativas: [
+            "Alternativas01",
+            "Alternativas02"
+        ]  
+    },
+    {
+        enunciado: "texto04",
+        alternativas: [
+            "Alternativas01",
+            "Alternativas02"
+        ]  
+    },
+    {
+        enunciado: "texto05",
+        alternativas: [
+            "Alternativas01",
+            "Alternativas02"
+        ]  
     }
 ]
 
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAternativas();
+}
+
+function mostraAternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas)
+    }
+}
