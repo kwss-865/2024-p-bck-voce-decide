035a0e63af16527844d0d5522e6745af93693c05
kwss-865 <134401209+kwss-865@users.noreply.github.com>
2024-08-15 08:32:38 -0300

    alternativas

--- main.js
+++ main.js
@@ -7,37 +7,62 @@ const textoResultado = document.querySelector('.texto-resultado');
 const perguntas = [
     {
         enunciado: "texto01",
-        alternativas: [
-            "Alternativas01",
-            "Alternativas02"
+        alternativas: [ {
+            texto:"Alternativas01",
+            afirmacao: "afirmação01"
+        },
+        {
+            texto:"Alternativas02",
+            afirmacao:"afirmção02"
+        }
         ]
     },
     {
         enunciado: "texto02",
-        alternativas: [
-            "Alternativas01",
-            "Alternativas02"
+        alternativas: [ {
+            texto:"Alternativas03",
+            afirmacao:"afirmação03"
+        },
+        {
+            texto:"Alternativas04",
+            afirmacao:"afirmação04"
+        }
         ]  
     },
     {
         enunciado: "texto03",
-        alternativas: [
-            "Alternativas01",
-            "Alternativas02"
+        alternativas: [ {
+            texto:"Alternativas05",
+            afirmacao:"afirmção05"
+        },
+        {
+            texto:"Alternativas06",
+            afirmacao:"afirmação06"
+        }
         ]  
     },
     {
         enunciado: "texto04",
-        alternativas: [
-            "Alternativas01",
-            "Alternativas02"
+        alternativas: [ {
+            texto:"Alternativas07",
+            afirmacao:"afirmação07"
+        },
+        {
+            texto:"Alternativas08",
+            afirmacao:"afirmação08"
+        }
         ]  
     },
     {
         enunciado: "texto05",
-        alternativas: [
-            "Alternativas01",
-            "Alternativas02"
+        alternativas: [ {
+            texto:"Alternativas09",
+            afirmacao:"afirmação09"
+        },
+        {
+            texto:"Alternativas10",
+            afirmacao:"afirmação10"
+        }
         ]  
     }
 ]
@@ -54,7 +79,12 @@ function mostraPergunta(){
 function mostraAternativas(){
     for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener('click',function(){
+            atual++;
+            mostraPergunta();
+        })
         caixaAlternativas.appendChild(botaoAlternativas)
     }
 }
+mostraPergunta();
